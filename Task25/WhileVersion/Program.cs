68668eb654acfb62dbab58691a54986b87d2fea3
Григорий Васякин <vxdll.39@gmail.com>
2022-09-15 22:43:21 +0200

    While version task25

---
+++ Task25/WhileVersion/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int result = A;
+while ( count < B){
+    result = A * result;
+    count = count + 1;
+}
+Console.WriteLine("A в степени B = " + result);
+
