d42a53dbfe71c7056764fb17a3b94e62e4f1a313
Григорий Васякин <vxdll.39@gmail.com>
2022-09-16 12:57:16 +0200

    Function

---
+++ Task25/Func/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Exp (int A, int B){
+    int result = 1;
+    for ( int i = 0; i < B; i++){
+        result = result * A;
+    }
+    return result;
+}
+int res = Exp(3,5);
+Console.WriteLine("A в степени B = " + res);
